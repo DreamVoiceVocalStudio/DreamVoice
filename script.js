62f2dd5406a1fe3ed36abf6c92054ff9facaa521
DreamVoiceVocalStudio <pukasenkoliza@gmail.com>
2026-08-31 18:52:31 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ let selectedDate = "";
 let selectedTime = "";
 let bookings = [];
 
-const API_URL = "https://script.google.com/macros/s/AKfycbwPI1VVfOQzuztBZ7EI8Z80AogKseDFaU-cp91CRsbX9ZQNKbyt_5zm1MSHUgGK0SIC_g/exec";
+const API_URL = "https://script.google.com/macros/s/AKfycbzrJKLGEJ7wbp-KuSJSkuOsIaQC9LTaFmjgHP9bD85eKOY4dapBARs4LBCI0ENIVQ17-g/exec";
 
 // ================================
 // ГРАФІК
